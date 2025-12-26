5b797246590af345143757d7683f36f2fdf59ea1
dnmnhie06 <dnmnhien06@gmail.com>
2025-12-26 16:02:37 +0700

    thembandoc

--- readers.py
+++ readers.py
@@ -1,4 +1,18 @@
 from data import readers, borrow_records
 from datetime import datetime
+# 1. Thêm bạn đọc
+def add_reader():
+    student_id = input("Nhập mã thẻ sinh viên: ")
+    name = input("Nhập họ tên sinh viên: ")
 
+    for r in readers:
+        if r["student_id"] == student_id:
+            print("❌ Thẻ sinh viên đã tồn tại")
+            return
+
+    readers.append({
+        "student_id": student_id,
+        "name": name
+    })
+    print("✔ Thêm bạn đọc thành công")
 
